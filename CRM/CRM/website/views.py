ea0d55019ddc6cbf4de46c13d720d9f1d1b7882e
Vladimir Nankov <102216547+vladymoney@users.noreply.github.com>
2023-08-17 23:07:01 +0300

    Added Records

--- CRM/CRM/website/views.py
+++ CRM/CRM/website/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
+
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -17,7 +21,7 @@ def home(request):
             messages.success(request, 'There Was An Error Loging In, Try Again')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 def logout_user(request):
