c80b629fe2a233a846443bd2d8900697218ae492
Vladimir Nankov <102216547+vladymoney@users.noreply.github.com>
2023-08-17 18:29:58 +0300

    Added Register

--- CRM/CRM/website/views.py
+++ CRM/CRM/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
@@ -23,3 +24,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You Have Been Logged Out...')
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You Have Successfuly Registered!')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'regster.html', {'form': form})
+
+
+    return render(request, 'regster.html', {'form': form})
