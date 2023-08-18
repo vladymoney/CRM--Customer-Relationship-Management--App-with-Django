ee74824e14a6f39ecfac7d2385f8473e2cb2789d
Vladimir Nankov <102216547+vladymoney@users.noreply.github.com>
2023-08-18 07:38:31 +0300

    Added Part Of Record Functionality

--- CRM/CRM/website/views.py
+++ CRM/CRM/website/views.py
@@ -46,3 +46,26 @@ def register_user(request):
 
 
     return render(request, 'regster.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, 'You Must Ne Logged In to View The Record')
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request,'Record Deleted Successfully')
+        return redirect('home')
+    else:
+        messages.success(request, 'You Must Be Logged In To do that')
+        return redirect('home')
+
+def add_record(request):
+    return render(request, 'add_record.html', {})
